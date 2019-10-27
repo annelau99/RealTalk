c6bd908d322e742ae47994219a2d5e54bead8625
kaitlynnhong <kaitlynnhong7@gmail.com>
2019-10-27 17:30:02 -0400

    added user input

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,29 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    String PhoneNumber, password;
+    EditText phoneInput;
+    EditText passInput;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phoneInput = (EditText) findViewById(R.id.phoneNum);
+        passInput = (EditText) findViewById(R.id.password);
+
+        submit = (Button) findViewById(R.id.submitbutton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhoneNumber = phoneInput.getText().toString();
+                password = passInput.getText().toString();
+
+                toastMe(v);
+            }
+        });
     }
+
+    /*public void login(View view){
+        phoneInput = (EditText) findViewById(R.id.phoneNum);
+        passInput = (EditText) findViewById(R.id.password);
+        submit = (Button) findViewById(R.id.submitbutton);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void OnClick(view) {
+                PhoneNumber = phoneInput.getText().toString();
+                password = passInput.getText().toString();
+
+                toastMe(view);
+            }
+        });
+    }*/
 
     public void toastMe(View view){
         // Toast myToast = Toast.makeText(this, message, duration);
-        Toast myToast = Toast.makeText(this, "Hello Toast!",
+        Toast myToast = Toast.makeText(this, "You submitted",
                 Toast.LENGTH_SHORT);
         myToast.show();
     }
 
-    public void countMe (View view) {
+
+    /*public void countMe (View view) {
         // Get the text view.
         TextView showCountTextView = (TextView)
-                findViewById(R.id.textView);
+                findViewById(R.id.welcome);
 
         // Get the value of the text view.
         String countString = showCountTextView.getText().toString();
@@ -48,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         Intent randomIntent = new Intent(this, SecondActivity.class);
 
         // Get the text view that shows the count.
-        TextView showCountTextView = (TextView) findViewById(R.id.textView);
+        TextView showCountTextView = (TextView) findViewById(R.id.welcome);
 
         // Get the value of the text view.
         String countString = showCountTextView.getText().toString();
@@ -61,6 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Start the new activity.
         startActivity(randomIntent);
-    }
+    }*/
 
 }
