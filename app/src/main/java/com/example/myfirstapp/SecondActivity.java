115afab660365fce75844fcb587e3c1801d5bd02
kaitlynnhong <kaitlynnhong7@gmail.com>
2019-12-01 21:09:34 -0500

    added checking if user enters information (main, new user registration)

--- app/src/main/java/com/example/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -4,71 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-//import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
 
-////    private String age, gender, sexualActivity, name, number;
+    private String age, gender, sexualActivity, name, number;
     private EditText ageInput, genderInput, sexInput,nameInput, numInput;
     private Button submit;
-//    private FirebaseAuth mFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-//        mFirebase = FirebaseAuth.getInstance();
-//        nameInput = (EditText) findViewById(R.id.editname);
-//        numInput = (EditText) findViewById(R.id.editnumber);
-//        ageInput = (EditText) findViewById(R.id.age_num);
-//        genderInput = (EditText) findViewById(R.id.gender_user);
-//        sexInput = (EditText) findViewById(R.id.sexact_user);
+
+        nameInput = (EditText) findViewById(R.id.editname);
+        numInput = (EditText) findViewById(R.id.editnumber);
+        ageInput = (EditText) findViewById(R.id.age_num);
+        genderInput = (EditText) findViewById(R.id.gender_user);
+        sexInput = (EditText) findViewById(R.id.sexact_user);
 
 
         submit = (Button) findViewById(R.id.submit_second_activity);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNextPage();
-            }
-        });
-/*
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name = nameInput.getText().toString();
-              number = numInput.getText().toString();
-               age = ageInput.getText().toString();
+                number = numInput.getText().toString();
+                age = ageInput.getText().toString();
                 gender = genderInput.getText().toString();
                 sexualActivity = sexInput.getText().toString();
-                if (name.isEmpty()){
-                    nameInput.setError("Please enter name!");
-                }
 
-                mFirebase.createUserWithEmailAndPassword(name, number).addOnCompleteListener(SecondActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            System.out.print("failed!");
-                            Toast.makeText(SecondActivity.this, "Failed!", Toast.LENGTH_SHORT);
-                        }
-                        else {
-                            openNextPage();
-                        }
-                    }
-                });
-
-                //the above function doesn't work, so just added this for demo purposes
-                openNextPage();
-
+                if (checkInputs())
+                    openNextPage();
             }
         });
-*/
+    }
+
+    //functions that check for input user info
+    public boolean checkInputs(){
+        if(TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (TextUtils.isEmpty(age)){
+            Toast.makeText(this, "Please enter your age", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (TextUtils.isEmpty(number)){
+            Toast.makeText(this, "Please enter your phone number", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (TextUtils.isEmpty(gender)){
+            Toast.makeText(this, "Please enter your gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (TextUtils.isEmpty(sexualActivity)){
+            Toast.makeText(this, "Please enter your sexual activity or health", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true; }
     }
 
     public void openNextPage() {
